658a3f34963b2eafd9706e1604ab9d7700a9a50f
maroontoruchang <maroon.toru.chang@gmail.com>
2022-12-05 17:51:20 +0900

    3章 練習問題2
    間違いだらけかもしれません。
    結果出力の所で500エラーが出ます。
    
    チェックお願いします。

--- PHPjob/3-3/index.php
+++ PHPjob/3-3/index.php
@@ -1,6 +1,6 @@
 <form action="result.php" method="get">
-  0〜9の適当な数字の羅列を入力してください
-  <input type="text" name="number" />
+  0〜9の適当な数字の羅列を入力してください。
   <br>
+  <input type="text" name="number" />
   <input type="submit" value="占う" />
 </form>
--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -1,28 +1,32 @@
 <?php
-$number = $_GET['number'];
+
+$number = $_POST["number"];
+
+$array = str_split($number);
+$keys = array_rand($array,1);
+$num = $array[$keys];
 $omikuji  = $num;
 $rand = [0,1,2,3,4,5,6,7,8,9];
 $rand = [mt_rand(0,9)];
 
-$num = rand(0,9);
- if($omikuji = 0){
+ if($omikuji == 0){
    echo "凶" ;
- }elseif ($omikuji > 3) {
+ }elseif ($omikuji <= 3) {
    echo "小吉";
- }elseif ($omikuji > 6) {
+ }elseif ($omikuji <= 6) {
    echo "中吉";
- }elseif ($omikuji > 8) {
+ }elseif ($omikuji <= 8) {
    echo "吉";
- }else ($omikuji = 9) {
+ }else ($omikuji == 9) {
      echo "大吉";
  }
 
 $today = date("Y-m-d");
 print_r($today);
 
+?>
+
 
 <p><?php echo $today; ?> のあなたの運勢 </p>
 <p>選ばれた数字は <?php echo $number; ?></p>
-<p><?php echo $romikuji; ?> です。</p>
-
-?>
+<p><?php echo $omikuji; ?> です。</p>
