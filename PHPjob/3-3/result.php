b6c13654138ed74a24f54aadc76d5b5e5ce9b86b
maroontoruchang <maroon.toru.chang@gmail.com>
2022-12-07 10:36:38 +0900

    3章 練習問題2
    指摘された部分、修正しました。

--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -1,13 +1,11 @@
 <?php
 
-$number = $_POST["number"];
+$number = $_GET["number"];
 
 $array = str_split($number);
 $keys = array_rand($array,1);
 $num = $array[$keys];
 $omikuji  = $num;
-$rand = [0,1,2,3,4,5,6,7,8,9];
-$rand = [mt_rand(0,9)];
 
  if($omikuji == 0){
    $unsei = "凶" ;
@@ -22,7 +20,6 @@ $rand = [mt_rand(0,9)];
  }
 
 $today = date("Y-m-d");
-print_r($today);
 
 ?>
 
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,20 +1,40 @@
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
+$name = $_POST['name'];
 
 //①画像を参考に問題文の選択肢の配列を作成してください。
 
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
 
 ?>
-<p>お疲れ様です<!--POST通信で送られてきた名前を出力-->さん</p>
+<p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $name; ?>さん</p>
 <!--フォームの作成 通信はPOST通信で-->
 <h2>①ネットワークのポート番号は何番？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+$port = ["80", "22", "21", "20"];
+
+foreach ($port as $value) { 
+    <input type="radio" name="port">
+    <?php echo $value;?>
+}
 
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+$weblanguage = ["PHP", "Python", "JAVA", "HTML"];
+
+foreach ($weblanguage as $value) { 
+    <input type="radio" name="weblanguage">
+    <?php echo $value;?>
+}
 
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+$mysql = ["join", "select", "insert", "update"];
+
+foreach ($mysql as $value) { 
+    <input type="radio" name="mysql">
+    <?php echo $value;?>
+}
 
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+$test_answer = $_POST['test_answer'];
